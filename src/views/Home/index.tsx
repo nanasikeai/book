6d833c2ad3ea7716e1c7d3ace36f3a2d25992aad
nanasikeai <nanasikeai@gmail.com>
2024-02-28 00:31:50 +0800

    feat: home（静态）

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,5 +1,25 @@
+import React from 'react'
+
+import s from './style.module.scss'
+import { ArrowDown } from '@react-vant/icons'
+
 const Home = () => {
-  return <div>Home</div>
+  return <div className={s.home}>
+    <div className={s.header}>
+      <div className={s.dataWrap}>
+        <span className={s.expense}>总支出：<b>¥ 208.00</b></span>
+        <span className={s.income}>总收入：<b>¥ 712.00</b></span>
+      </div>
+      <div className={s.typeWrap}>
+        <div className={s.left}>
+          <span className={s.title}>全部类型</span>
+        </div>
+        <div className={s.right}>
+          <span className={s.time}>2022-06</span>
+        </div>
+      </div>
+    </div>
+  </div>
 }
 
 export default Home
